ff74a12f8d1c8d0a079e3128e99ce8a7f1c95af8
Danshaning <danshaning@gmail.com>
2022-11-22 10:23:30 -0600

    Add project files.

---
+++ Homework1_ShaningA/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Homework1_ShaningA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.myName = new System.Windows.Forms.Label();
+            this.lblAboutMe = new System.Windows.Forms.Label();
+            this.btnMore = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Homework1_ShaningA.Properties.Resources.onepiece;
+            this.pictureBox4.Location = new System.Drawing.Point(457, 274);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(341, 174);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Homework1_ShaningA.Properties.Resources.myIdol;
+            this.pictureBox3.Location = new System.Drawing.Point(1, 264);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(341, 184);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Homework1_ShaningA.Properties.Resources.dreambody;
+            this.pictureBox2.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(341, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Homework1_ShaningA.Properties.Resources.afrBenin;
+            this.pictureBox1.Location = new System.Drawing.Point(457, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // myName
+            // 
+            this.myName.Font = new System.Drawing.Font("Segoe Script", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myName.ForeColor = System.Drawing.Color.Brown;
+            this.myName.Location = new System.Drawing.Point(208, 2);
+            this.myName.Name = "myName";
+            this.myName.Size = new System.Drawing.Size(345, 81);
+            this.myName.TabIndex = 5;
+            this.myName.Text = "Shaning Daniel AYAH";
+            this.myName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAboutMe
+            // 
+            this.lblAboutMe.Font = new System.Drawing.Font("Segoe Script", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAboutMe.ForeColor = System.Drawing.Color.Chartreuse;
+            this.lblAboutMe.Location = new System.Drawing.Point(133, 161);
+            this.lblAboutMe.Name = "lblAboutMe";
+            this.lblAboutMe.Size = new System.Drawing.Size(496, 141);
+            this.lblAboutMe.TabIndex = 6;
+            this.lblAboutMe.Text = "I\'m from Benin and my favorite hobbies are watching anime, working out, and check" +
+    "ing on what new stuff our civilization discovered about space. ";
+            this.lblAboutMe.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btnMore
+            // 
+            this.btnMore.BackColor = System.Drawing.SystemColors.Info;
+            this.btnMore.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMore.Location = new System.Drawing.Point(348, 345);
+            this.btnMore.Name = "btnMore";
+            this.btnMore.Size = new System.Drawing.Size(103, 56);
+            this.btnMore.TabIndex = 7;
+            this.btnMore.Text = "Clic for more..";
+            this.btnMore.UseVisualStyleBackColor = false;
+            this.btnMore.Click += new System.EventHandler(this.btnMore_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(1, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(805, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMore);
+            this.Controls.Add(this.lblAboutMe);
+            this.Controls.Add(this.myName);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.SeaGreen;
+            this.Name = "Form1";
+            this.Text = "This Is Me";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label myName;
+        private System.Windows.Forms.Label lblAboutMe;
+        private System.Windows.Forms.Button btnMore;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Homework1_ShaningA/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1_ShaningA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMore_Click(object sender, EventArgs e)
+        {
+            lblAboutMe.Text = "Also I got  into IT 3 years ago because it is just the future. I'm a full time student at MVCC and I'm graduating this semester";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
